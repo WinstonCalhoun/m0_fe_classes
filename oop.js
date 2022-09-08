ae090ed1750b95f179ce5942036f1a5966f9a3fb
Winston Calhoun <superdevant@gmail.com>
2022-09-08 17:39:23 -0500

    Initial commit

--- oop.js
+++ oop.js
@@ -3,6 +3,22 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn {
+  constructor (name) {
+    this.name = name;
+    this.color = "silver";
+    this.legs = 4;
+    this.horn = 1;
+  }
+  say(string) {
+    console.log('*~*${string}*~*');
+  }
+};
+
+var unicorn1 = new Unicorn("Charlie");
+console.log(unicorn1);
+
+unicorn1.say("sprakles")
 
 
 // Write a class called Vampire
@@ -10,6 +26,25 @@
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
+
+class Vampire {
+  constructor (name, pet) {
+    this.name = name;
+    this.pet = (pet || "bat");
+    this.thirsty = true
+  }
+  drink(changeThirsty) {
+      this.thirsty = false;
+  }
+};
+
+var vampire1 = new Vampire("Ryan", "spider");
+vampire1.drink();
+  console.log(vampire1);
+
+
+var vampire2 = new Vampire("Brad", "cat");
+  console.log(vampire2);
 
 
 
@@ -21,6 +56,15 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+class Dragon {
+  constructor (name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = "red";
+    this.isHungry = true
+    for (i = 0; i >= 4; )
+  }
+}
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+// More difficult that i remember.  Most parts were easier until the end where it is stuff less practiced.
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+// Workflow is going well.  Taking correct amount of breaks to be able to come back and stay focused for set amoun of time.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+// Seems like a regular job.  Every 2 hours a 10 minute.
